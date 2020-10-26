c1cc7adabeed10eae073367c8c575ec4b1ab293b
Simran <simran.maurya_cs18@gla.ac.in>
2020-10-26 16:02:41 +0530

    let and const types

---
+++ JAVASCRIPT/let_const.js
@@ -0,0 +1,22 @@
+/*
+const a= 10;
+a++;   //not allowed
+*/
+/*
+{                   //local scope
+    let a=10;
+    console.log(a);
+}
+console.log(a);      
+*/
+/*
+{
+    var b=20;       //functional scope not block scope
+    let a=10;
+    console.log(a);
+}
+console.log(b);
+*/
+//you can't create a block var. without using let
+b= 20;
+console.log(b);
