435fe2e41aa4e0590774ddb250e3c2b0f852ef16
Simran <simran.maurya_cs18@gla.ac.in>
2020-11-24 21:06:52 +0530

    Practical-18(Form validation)

--- JAVASCRIPT/Practical-19(Form Validation)/main.js
+++ JAVASCRIPT/Practical-19(Form Validation)/main.js
@@ -9,7 +9,7 @@ const small= document.querySelector('small');
 
 //event handler
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit',function(e){ //anonymous function
     e.preventDefault();  //stops data submission to server because we want to validate it first
     checkInput();
 });
@@ -25,27 +25,30 @@ function checkInput()
     // console.log(usernameValue);
     if(usernameValue === '')
     {
-        showError(usernameValue,"Username can't be blank");
+        showError(username,"Username can't be blank");
+    }
+    else if(!isUsernameValid(usernameValue))
+    {
+        showError(username,"Username is not valid");
     }
     else{
-        showSuccess(usernameValue);
+        showSuccess(username);
     }
 
     if(emailValue === '')
     {
         showError(email,"Emailid can't be blank");
     }
-    else{
-        showSuccess(emailValue);
-    }
     else if(!isEmailValid(emailValue))
     {
         showError(email,"Emailid is not valid");
     }
-
+    else{
+        showSuccess(email);
+    }
     if(pass1Value === '')
     {
-        showError(pass1Value,"Password can't be blank");
+        showError(pass1,"Password can't be blank");
     }
     else{
         showSuccess(pass1);
@@ -53,11 +56,11 @@ function checkInput()
     
     if(pass2Value === '')
     {
-        showError(pass2Value,"Password can't be blank");
+        showError(pass2,"Password can't be blank");
     }
     else if(pass2Value != pass1Value)
     {
-        showError(pass2Value,"Passwords not matched");
+        showError(pass2,"Passwords not matched");
     }
     else{
         showSuccess(pass2);
@@ -70,18 +73,20 @@ function  showError(input,msg)
     const formControl= input.parentNode;
     formControl.className= 'form-control error';
     const small= formControl.querySelector('small');
-    small.innerHTML = 
+    small.innerHTML = msg;
 
 }
-function  showSuccess(input,msg)
+function  showSuccess(input)
 {
     const formControl= input.parentNode;
     formControl.className= 'form-control success';
-
 }
 function isEmailValid(email)
 {
     return /^([A-Za-z0-9_\.\-]+)@([A-Za-z0-9]+)\.([A-Za-z]{2,3})$/.test(email);  //2 for in , 3 for com
 }
-
+function isUsernameValid(username)
+{
+    return /^([a-z]+)$/.test(username);
+}
 //Practical-19 username can't be capital letter and can't contain special symbols 
